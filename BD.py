a4e4731d063dcad333df44e5cd5f0548ef1d0c9e
Alexei-Python <stanoktuta@gmail.com>
2021-03-27 21:04:00 +0300

    connect BD

---
+++ BD.py
@@ -0,0 +1,18 @@
+import pymysql
+import pymysql.cursors
+
+connect = pymysql.connect(
+                          host = 'localhost',\
+                          user = 'stanok',\
+                          password = 'cnfyjr11101984',\
+                          database = 'stanok',\
+                          cursorclass = pymysql.cursors.DictCursor)
+
+cursor = connect.cursor()
+
+
+#sql = 'SELECT * FROM people'
+#cursor.execute(sql)
+#data = cursor.fetchall()
+#for element in data:
+#    print(element['login'])
---
+++ registration/Newlogin.py
@@ -0,0 +1,2 @@
+class Registr:
+    pass
