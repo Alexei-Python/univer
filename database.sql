585f24669ff070bb57ba12859a51f0a3f1670068
Alexei-Python <stanoktuta@gmail.com>
2021-04-17 17:20:11 +0300

    add more beauty

--- database.sql
+++ database.sql
@@ -10,12 +10,14 @@ CREATE TABLE people (
     PRIMARY KEY(ID)
     );
 CREATE TABLE progress (
+    ID INT(100) AUTO_INCREMENT,
     login VARCHAR(20),
     course INT(6),
     fuckulty VARCHAR(30),
     physic INT(10),
     math INT(10),
     python INT(10),
-    lang INT(10)
+    lang INT(10),
+    PRIMARY KEY(ID)
     );
 
